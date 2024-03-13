1f7b0307e18701ccf65fe9d9567d4469f81ec9f5
hgkim0728 <hgkim0728@gmail.com>
2024-03-13 19:50:06 +0900

    2024.03.13

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -8,10 +8,14 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float jumpForce = 20.0f;
 
     private Rigidbody2D rigid;
+    private CapsuleCollider2D capsuleCollider;
+
+    [SerializeField]private bool isGround = false;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Start()
@@ -35,9 +39,25 @@ public class PlayerMove : MonoBehaviour
 
     private void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Physics2D.Raycast(transform.position, Vector2.down, capsuleCollider.size.y / 2 + 0.3f, LayerMask.GetMask("Ground")))
+        {
+            isGround = true;
+        }
+        Debug.DrawRay(transform.position, new Vector3(0, -capsuleCollider.size.y / 2 - 0.1f), Color.red);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGround == true)
         {
             rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
         }
+    }
+
+    // 화면 비율 맞추기 코드
+    private void S()
+    {
+        float targetRatio = 9f / 16f;
+        float ratio = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = ratio / targetRatio;
+        float fixedWidth = (float)Screen.width / scaleHeight;
+        Screen.SetResolution((int)fixedWidth, Screen.height, true);
     }
 }
