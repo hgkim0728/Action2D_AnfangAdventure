bbf95c28a3c7459322a2f95af650bbaedb61ac0e
hgkim0728 <hgkim0728@gmail.com>
2024-05-04 18:51:06 +0900

    2024.05.04

--- Assets/02.Scripts/Slot.cs
+++ Assets/02.Scripts/Slot.cs
@@ -9,7 +9,16 @@ public class Slot : MonoBehaviour
     ItemImg itemImg;
     Item item;  // 슬롯 안에 있는 아이템의 정보
     int slotIdx = 0;    // 리스트에 들어간 순서
+    public int SlotIdx
+    {
+        get { return slotIdx; }
+        set { slotIdx = value; }
+    }
     int itemCount = 0;  // 슬롯 안에 있는 아이템의 수
+    public int ItemCount
+    {
+        set { itemCount = value; }
+    }
 
     private void Start()
     {
