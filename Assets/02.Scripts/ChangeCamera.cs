8d42f945125c4384a9cd619a780ba0ea1ea51ab5
hgkim0728 <hgkim0728@gmail.com>
2024-09-28 17:47:38 +0900

    스테이지 별 카메라 이동

---
+++ Assets/02.Scripts/ChangeCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject virtualCamera;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCamera.SetActive(false);
+        }
+    }
+}
--- Assets/02.Scripts/NextStage.cs
+++ Assets/02.Scripts/NextStage.cs
@@ -6,15 +6,11 @@ using UnityEngine;
 public class NextStage : MonoBehaviour
 {
     [SerializeField] private Transform exit;
-    [SerializeField] private PolygonCollider2D polyCol;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            CinemachineConfiner2D cine = 
-                GameObject.Find("Virtual Camera").GetComponent<CinemachineConfiner2D>();
-            cine.m_BoundingShape2D = polyCol;
             collision.transform.position = exit.transform.position;
         }
     }
